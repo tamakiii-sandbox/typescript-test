cd460977c97373bb0b2b027d4bfbbd8a0e392651
tamakiii <tamakiii@users.noreply.github.com>
2019-05-21 22:56:08 +0900

    add packages

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
+include package.mk
 
 init: \
 	package.json
 
+clean:
+	rm package.json
+
 package.json:
 	npm init --yes
+	npm install $(PACKAGES)
 
---
+++ package.mk
@@ -0,0 +1,21 @@
+PACKAGES := \
+	webpack
+	# @types/node \
+	# @types/react \
+	# @types/react-dom \
+	# @types/react-virtualized \
+	# @types/styled-components \
+	# css-loader \
+	# html-webpack-plugin \
+	# react \
+	# react-dom \
+	# react-test-renderer \
+	# react-virtualized \
+	# source-map-support \
+	# style-loader \
+	# styled-components \
+	# ts-loader \
+	# typescript \
+	# webpack \
+	# webpack-cli \
+	# webpack-dev-server
